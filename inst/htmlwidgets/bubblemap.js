27999e8e3d4965a1f0793bf5aaf12a373953e4f9
David Carlson <dacarlson@mac.com>
2015-10-04 17:31:57 -0500

    added initialization logic

--- inst/htmlwidgets/bubblemap.js
+++ inst/htmlwidgets/bubblemap.js
@@ -5,61 +5,42 @@ HTMLWidgets.widget({
   type: 'output',
 
   initialize: function(el, width, height) {
-    return {
-      width: width, height: height
-    }
-  },
+    var states = HTMLWidgets.getAttachmentUrl('shapes', 'states');
 
-  renderValue: function(el, x, instance) {
+    var sizes = [10000, 100000, 250000];
 
-    // hack for now, need to separate map initialization and data population
-    d3.select(el).empty()
-
-    var states = HTMLWidgets.getAttachmentUrl('shapes', 'states'),
-        data = HTMLWidgets.dataframeToD3(x.data),
-        sizes = [10000, 100000, 250000];
-
-    var container = d3.select(el),
-        width = instance.width,
-        height = instance.height;
+    var container = d3.select(el);
 
     var projection = d3.geo.albersUsa()
-      .scale(1100)
-      .translate([width / 2, height / 2]);
+        .scale(1100)
+        .translate([width / 2, height / 2]);
 
     var path = d3.geo.path()
-      .projection(projection);
+        .projection(projection);
 
     var svg = container
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "map-container");
 
     var g = svg.append("g");
 
-    g.append( "rect" )
+    svg.append( "rect" )
         .attr("width",width)
         .attr("height",height)
         .attr("fill","white")
         .attr("opacity",0)
-        .on("mouseover",function(){
-            hoverData = null;
-            if ( probe ) probe.style("display","none");
-        });
+        .attr("class", "background");
 
-    var map = g.append("g")
-        .attr("class", "map");
+    var map = svg.append("g").attr("class", "map");
 
-    var probe, hoverData;
+    svg.append("g").attr("class", "data");
+
+    var probe;
 
     probe = container.append("div")
-      .attr("class","probe");
-
-    var format = d3.format(",");
-
-    function circleSize(d){
-        return Math.sqrt( .02 * Math.abs(d) );
-    };
+        .attr("class","probe");
 
     d3.json(states, function(error, us) {
         map.selectAll("path")
@@ -72,61 +53,90 @@ HTMLWidgets.widget({
 
         map.append("path")
             .datum(topojson.mesh(us, us.objects.states, function(a, b) {
-               return a !== b; }))
+                return a !== b; }))
             .attr("class", "state-boundary")
             .attr("vector-effect","non-scaling-stroke")
             .attr("d", path);
 
-         // draw city points
-        for ( var i in data ){
-            var projected = projection([ parseFloat(data[i].LON), parseFloat(data[i].LAT) ]);
-            map.append("circle")
-                .datum( data[i] )
-                .attr("cx",projected[0])
-                .attr("cy",projected[1])
-                .attr("r",1)
-                .attr("vector-effect","non-scaling-stroke")
-                .on("mousemove", function(d){
-                    hoverData = d;
-                    setProbeContent(d);
-                    probe
-                        .style( {
-                            "display" : "block",
-                            "top" : (d3.event.pageY - 80) + "px",
-                            "left" : (d3.event.pageX + 10) + "px"
-                        });
-                })
-                .on("mouseout",function(){
-                    hoverData = null;
-                    probe.style("display","none");
-                });
+        // create legend
+        var legend = svg.append("g").attr("class","legend").attr("transform","translate(560,10)");
+
+        legend.append("circle").attr("class","gain").attr("r",5).attr("cx",5).attr("cy",10);
+        legend.append("circle").attr("class","loss").attr("r",5).attr("cx",5).attr("cy",30);
+
+        legend.append("text").text("gained").attr("x",15).attr("y",13);
+        legend.append("text").text("lost").attr("x",15).attr("y",33);
+
+        for ( var i in sizes ){
+            legend.append("circle")
+                .attr( "r", circleSize( sizes[i] ) )
+                .attr( "cx", 80 + circleSize( sizes[sizes.length-1] ) )
+                .attr( "cy", 2 * circleSize( sizes[sizes.length-1] ) - circleSize( sizes[i] ) )
+                .attr("vector-effect","non-scaling-stroke");
+            legend.append("text")
+                .text( (sizes[i] / 1000) + "K" + (i == sizes.length-1 ? "" : "") )
+                .attr( "text-anchor", "middle" )
+                .attr( "x", 80 + circleSize( sizes[sizes.length-1] ) )
+                .attr( "y", 2 * ( circleSize( sizes[sizes.length-1] ) - circleSize( sizes[i] ) ) + 5 )
+                .attr( "dy", 13)
         }
+    });
 
-    // create legend
-    var legend = g.append("g").attr("class","legend").attr("transform","translate(560,10)");
+        return {
+            projection: projection
+        }
+  },
 
-    legend.append("circle").attr("class","gain").attr("r",5).attr("cx",5).attr("cy",10);
-    legend.append("circle").attr("class","loss").attr("r",5).attr("cx",5).attr("cy",30);
+  renderValue: function(el, x, instance) {
 
-    legend.append("text").text("gained").attr("x",15).attr("y",13);
-    legend.append("text").text("lost").attr("x",15).attr("y",33);
+    var data = HTMLWidgets.dataframeToD3(x.data);
 
-    for ( var i in sizes ){
-        legend.append("circle")
-            .attr( "r", circleSize( sizes[i] ) )
-            .attr( "cx", 80 + circleSize( sizes[sizes.length-1] ) )
-            .attr( "cy", 2 * circleSize( sizes[sizes.length-1] ) - circleSize( sizes[i] ) )
-            .attr("vector-effect","non-scaling-stroke");
-        legend.append("text")
-            .text( (sizes[i] / 1000) + "K" + (i == sizes.length-1 ? "" : "") )
-            .attr( "text-anchor", "middle" )
-            .attr( "x", 80 + circleSize( sizes[sizes.length-1] ) )
-            .attr( "y", 2 * ( circleSize( sizes[sizes.length-1] ) - circleSize( sizes[i] ) ) + 5 )
-            .attr( "dy", 13)
+    var projection = instance.projection;
+
+    var container = d3.select(el),
+        map = container.select(".map"),
+        dataLayer = container.select(".data"),
+        probe = container.select(".probe");
+
+    var hoverData;
+    var format = d3.format(",");
+
+    // clear existing points
+    dataLayer.selectAll('circle').remove();
+
+    container.select(".background")
+        .on("mouseover",function(){
+        hoverData = null;
+        if ( probe ) probe.style("display","none");
+    });
+
+    // draw city points
+    for ( var i in data ){
+        var projected = projection([ parseFloat(data[i].LON), parseFloat(data[i].LAT) ]);
+        dataLayer.append("circle")
+            .datum( data[i] )
+            .attr("cx",projected[0])
+            .attr("cy",projected[1])
+            .attr("r",1)
+            .attr("vector-effect","non-scaling-stroke")
+            .on("mousemove", function(d){
+                hoverData = d;
+                setProbeContent(d);
+                probe
+                    .style( {
+                        "display" : "block",
+                        "top" : (d3.event.pageY - 80) + "px",
+                        "left" : (d3.event.pageX + 10) + "px"
+                    });
+            })
+            .on("mouseout",function(){
+                hoverData = null;
+                probe.style("display","none");
+            });
     }
 
     // initalize map
-    var circle = map.selectAll("circle")
+    var circle = dataLayer.selectAll("circle")
         .sort(function(a,b){
             // catch nulls, and sort circles by size (smallest on top)
             if ( isNaN(a.VALUE) ) a.VALUE = 0;
@@ -144,8 +154,6 @@ HTMLWidgets.widget({
         setProbeContent(hoverData);
     }
 
-    });
-
     function setProbeContent(d){
         var val = d.VALUE;
         var html = "<strong>" + d.LABEL + "</strong><br/>" +
@@ -160,3 +168,7 @@ HTMLWidgets.widget({
   }
 
 });
+
+function circleSize(d){
+    return Math.sqrt( .02 * Math.abs(d) );
+}
